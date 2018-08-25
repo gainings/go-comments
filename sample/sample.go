f3f7e86ae932b60e9e81de512fa7d8fed41c80f8
gainings <gain.beats@gmail.com>
2018-08-25 22:54:39 +0900

    Add sample file with no comments

---
+++ sample/sample.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+const (
+	Hoge = "Hoge"
+	hoge = "hoge"
+	Hage = "Hage"
+)
+
+type Fuga struct {
+	Name string
+}
+
+type fuga struct {
+	name string
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
+
+func Foo() {
+
+}
+
+func bar() {
+
+}
